5b1bd100c6e86cb6f8cf2488df83cff6f7ad10a2
davesmith.gbs <davesmith.gbs@gmail.com>
2014-05-28 09:33:08 +0000

    work towards 1.0

--- buttermilk/src/main/java/com/cryptoregistry/ec/ECKeyContents.java
+++ buttermilk/src/main/java/com/cryptoregistry/ec/ECKeyContents.java
@@ -1,19 +1,55 @@
 package com.cryptoregistry.ec;
 
 import java.math.BigInteger;
+import java.util.UUID;
 
+import x.org.bouncycastle.crypto.params.ECDomainParameters;
+import x.org.bouncycastle.crypto.params.ECPrivateKeyParameters;
 import x.org.bouncycastle.math.ec.ECPoint;
 
-public class ECKeyContents {
+public class ECKeyContents extends ECKeyForPublication {
 	
 	public final BigInteger d;
-	public final ECPoint Q;
-	public String curveName;
-
-	public ECKeyContents(String name, BigInteger d, ECPoint Q) {
-		this.curveName = name;
+	
+	public ECKeyContents(ECPoint q, String curveName, BigInteger d) {
+		super(UUID.randomUUID().toString(), q, curveName);
 		this.d = d;
-		this.Q = Q;
+	}
+
+	public ECKeyContents(String handle, ECPoint q, String curveName, BigInteger d) {
+		super(handle, q, curveName);
+		this.d = d;
+	}
+	
+	public ECPrivateKeyParameters getPrivateKey() {
+		ECDomainParameters domain = CurveFactory.getCurveForName(curveName);
+		 ECPrivateKeyParameters params = new ECPrivateKeyParameters(d, domain);
+		 return params;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((d == null) ? 0 : d.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ECKeyContents other = (ECKeyContents) obj;
+		if (d == null) {
+			if (other.d != null)
+				return false;
+		} else if (!d.equals(other.d))
+			return false;
+		return true;
 	}
 
 }
