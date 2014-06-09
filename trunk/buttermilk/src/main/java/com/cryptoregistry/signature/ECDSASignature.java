fe06f88cfc19ed24f5dcdd4f02c041424773f055
davesmith.gbs@gmail.com <davesmith.gbs@gmail.com@c95ee3aa-bd1a-c76a-db35-d8dc5d9f496b>
2014-06-09 03:06:14 +0000

    work towards 1.0 - ESDSA signature

---
+++ trunk/buttermilk/src/main/java/com/cryptoregistry/signature/ECDSASignature.java
@@ -0,0 +1,47 @@
+package com.cryptoregistry.signature;
+
+import java.math.BigInteger;
+
+public class ECDSASignature {
+	
+	public final BigInteger r;
+	public final BigInteger s;
+	
+	public ECDSASignature(BigInteger r, BigInteger s) {
+		super();
+		this.r = r;
+		this.s = s;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((r == null) ? 0 : r.hashCode());
+		result = prime * result + ((s == null) ? 0 : s.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ECDSASignature other = (ECDSASignature) obj;
+		if (r == null) {
+			if (other.r != null)
+				return false;
+		} else if (!r.equals(other.r))
+			return false;
+		if (s == null) {
+			if (other.s != null)
+				return false;
+		} else if (!s.equals(other.s))
+			return false;
+		return true;
+	}
+
+}
