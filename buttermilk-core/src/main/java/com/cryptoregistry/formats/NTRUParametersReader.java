6fcee12ff23d3b8e7e752cc2f9a38a29c27992e3
davesmith.gbs <davesmith.gbs@gmail.com>
2014-06-29 05:20:41 +0000

    adding bc's NTRU

---
+++ buttermilk-core/src/main/java/com/cryptoregistry/formats/NTRUParametersReader.java
@@ -0,0 +1,61 @@
+package com.cryptoregistry.formats;
+
+import java.util.Map;
+
+import com.cryptoregistry.util.ArmoredString;
+
+import x.org.bouncycastle.crypto.Digest;
+import x.org.bouncycastle.crypto.digests.SHA256Digest;
+import x.org.bouncycastle.crypto.digests.SHA512Digest;
+import x.org.bouncycastle.pqc.crypto.ntru.NTRUEncryptionParameters;
+
+
+/**
+ * Parse what NTRUParametersFormatter can write
+ * 
+ * @author Dave
+ *
+ */
+public class NTRUParametersReader {
+
+	private final Map<String,Object> input;
+	
+	public NTRUParametersReader(Map<String,Object> input) {
+		this.input = input;
+	}
+	
+	public NTRUEncryptionParameters parse() {
+		
+	//	int PolyType = Integer.parseInt(String.valueOf(input.get("PolyType")));
+		int N = Integer.parseInt(String.valueOf(input.get("N")));
+		int q = Integer.parseInt(String.valueOf(input.get("q")));
+		int df = Integer.parseInt(String.valueOf(input.get("df")));
+		int df1 = Integer.parseInt(String.valueOf(input.get("df1")));
+		int df2 = Integer.parseInt(String.valueOf(input.get("df2")));
+		int df3 = Integer.parseInt(String.valueOf(input.get("df3")));
+		int db = Integer.parseInt(String.valueOf(input.get("db")));
+		int dm0 = Integer.parseInt(String.valueOf(input.get("dm0")));
+		int c = Integer.parseInt(String.valueOf(input.get("c")));
+		int minCallsR = Integer.parseInt(String.valueOf(input.get("minCallsR")));
+		int minCallsMask = Integer.parseInt(String.valueOf(input.get("minCallsMask")));
+		boolean hashSeed = Boolean.parseBoolean(String.valueOf(input.get("hashSeed")));
+		byte [] oid = new ArmoredString(String.valueOf(input.get("oid"))).decodeToBytes();
+		boolean sparse = Boolean.parseBoolean(String.valueOf(input.get("sparse")));
+		boolean fastFp = Boolean.parseBoolean(String.valueOf(input.get("fastFp")));
+		String digestAlg = String.valueOf(input.get("DigestAlgorithm"));
+		Digest digest = null;
+		if(digestAlg.equals("SHA-256")){
+			digest = new SHA256Digest();
+		}else{
+			digest = new SHA512Digest();
+		}
+		
+		// works because the likelihood of ones appearing the polynomials is rather small
+		if(df1 == 0 && df2==0 && df3== 0){
+			return new NTRUEncryptionParameters(N, q, df, dm0, db, c, minCallsR, minCallsMask, hashSeed, oid, sparse, fastFp, digest);
+		}else{
+			return new NTRUEncryptionParameters(N, q, df1, df2, df3, dm0, db, c, minCallsR, minCallsMask, hashSeed, oid, sparse, fastFp, digest);
+		}
+	}
+
+}
--- buttermilk-core/src/test/java/com/cryptography/ntru/CryptoFactoryTest.java
+++ buttermilk-core/src/test/java/com/cryptography/ntru/CryptoFactoryTest.java
@@ -7,12 +7,18 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 
+import x.org.bouncycastle.pqc.math.ntru.polynomial.DenseTernaryPolynomial;
+import x.org.bouncycastle.pqc.math.ntru.polynomial.IntegerPolynomial;
+import x.org.bouncycastle.pqc.math.ntru.polynomial.ProductFormPolynomial;
+import x.org.bouncycastle.pqc.math.ntru.polynomial.SparseTernaryPolynomial;
 import x.org.bouncycastle.util.Arrays;
 
 import com.cryptoregistry.formats.JSONBuilder;
 import com.cryptoregistry.ntru.CryptoFactory;
 import com.cryptoregistry.ntru.NTRUKeyContents;
 import com.cryptoregistry.ntru.NTRUNamedParameters;
+import com.cryptoregistry.util.ArmoredCompressedString;
+import com.cryptoregistry.util.ArrayUtil;
 
 public class CryptoFactoryTest {
 
@@ -43,6 +49,9 @@ public class CryptoFactoryTest {
 	@Test
 	public void test2() {
 		
+		byte [] in = "Hello NTRU world".getBytes(Charset.forName("UTF-8"));
+		NTRUKeyContents dup_skey = null;
+		
 		for(NTRUNamedParameters p: NTRUNamedParameters.values()){
 			System.out.println(p.toString());
 			NTRUKeyContents sKey = CryptoFactory.INSTANCE.generateKeys(p.getKeyGenerationParameters());
@@ -53,6 +62,38 @@ public class CryptoFactoryTest {
 			StringWriter writer = new StringWriter();
 			f.format(writer);
 			System.err.println(writer.toString());
+			
+			ArmoredCompressedString _h = sKey.wrappedH();
+			IntegerPolynomial h = new IntegerPolynomial(ArrayUtil.unwrapIntArray(_h));
+			ArmoredCompressedString _fp = sKey.wrappedFp();
+			IntegerPolynomial fp = new IntegerPolynomial(ArrayUtil.unwrapIntArray(_fp));
+			Object obj = sKey.wrappedT();
+			if(obj.getClass().isArray()){
+				// product form
+				ArmoredCompressedString [] array = (ArmoredCompressedString[])obj;
+				SparseTernaryPolynomial t0 = new SparseTernaryPolynomial(ArrayUtil.unwrapIntArray(array[0]));
+				SparseTernaryPolynomial t1 = new SparseTernaryPolynomial(ArrayUtil.unwrapIntArray(array[1]));
+				SparseTernaryPolynomial t2 = new SparseTernaryPolynomial(ArrayUtil.unwrapIntArray(array[2]));
+				ProductFormPolynomial t = new ProductFormPolynomial(t0,t1,t2);
+				dup_skey = new NTRUKeyContents(sKey.metadata,sKey.params,h,t,fp);
+			}else{
+				if(sKey.params.sparse){
+					// sparse ternary
+					ArmoredCompressedString acs = (ArmoredCompressedString)obj;
+					SparseTernaryPolynomial t = new SparseTernaryPolynomial(ArrayUtil.unwrapIntArray(acs));
+					dup_skey = new NTRUKeyContents(sKey.metadata,sKey.params,h,t,fp);
+				}else{
+					// dense ternary
+					ArmoredCompressedString acs = (ArmoredCompressedString)obj;
+					DenseTernaryPolynomial t = new DenseTernaryPolynomial(ArrayUtil.unwrapIntArray(acs));
+					dup_skey = new NTRUKeyContents(sKey.metadata,sKey.params,h,t,fp);
+				}
+			}
+			
+			byte [] encrypted = CryptoFactory.INSTANCE.encrypt(dup_skey, in);
+			byte [] out = CryptoFactory.INSTANCE.decrypt(dup_skey, encrypted);
+			Assert.assertTrue(Arrays.areEqual(in, out));
+			
 		}
 	}
 
