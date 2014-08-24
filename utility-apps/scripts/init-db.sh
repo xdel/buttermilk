e7e7244ce1a5e53a9c5c94d81f26eb8080192a4b
davesmith.gbs <davesmith.gbs@gmail.com>
2014-08-24 10:57:16 +0000

    on the road to 1.0

---
+++ utility-apps/scripts/init-db.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# use our local .m2 as our repo - we assume you have built the code
+bracket=~/.m2/repository/asia/redact/bracket/properties/bracket-properties/1.3.6/bracket-properties-1.3.6.jar
+jcore=~/.m2/repository/com/fasterxml/jackson/core/jackson-core/2.2.3/jackson-core-2.2.3.jar
+jdatabind=~/.m2/repository/com/fasterxml/jackson/core/jackson-databind/2.2.3/jackson-databind-2.2.3.jar
+jannotations=~/.m2/repository/com/fasterxml/jackson/core/jackson-annotations/2.2.3/jackson-annotations-2.2.3.jar
+bcore=~/.m2/repository/com/cryptoregistry/buttermilk-core/1.0.0/buttermilk-core-1.0.0.jar
+base64=~/.m2/repository/net/iharder/base64/2.3.8/base64-2.3.8.jar
+client=~/.m2/repository/com/cryptoregistry/client-storage/1.0.0/client-storage-1.0.0.jar
+utilapps=~/.m2/repository/com/cryptoregistry/utility-apps/1.0.0/utility-apps-1.0.0.jar
+
+CLASSPATH=$bracket:$jcore:$jdatabind:$jannotations:$bcore:$base64:$client:$utilapps
+
+#echo "$(cygpath -pw "$CLASSPATH")" 
+echo 
+java -cp "$(cygpath -pw "$CLASSPATH")" com.cryptoregistry.utility.app.InitDBApp "$@"
