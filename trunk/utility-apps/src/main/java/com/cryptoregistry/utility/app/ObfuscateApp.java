466e0a7513691be9abf353735891bd584c365fdd
davesmith.gbs@gmail.com <davesmith.gbs@gmail.com@c95ee3aa-bd1a-c76a-db35-d8dc5d9f496b>
2014-09-28 09:21:33 +0000

    work on ui

--- trunk/utility-apps/src/main/java/com/cryptoregistry/utility/app/ObfuscateApp.java
+++ trunk/utility-apps/src/main/java/com/cryptoregistry/utility/app/ObfuscateApp.java
@@ -2,6 +2,7 @@ package com.cryptoregistry.utility.app;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 
 import asia.redact.bracket.properties.Obfuscate;
@@ -16,19 +17,19 @@ import com.cryptoregistry.util.CmdLineParser.OptionException;
 import com.cryptoregistry.util.ShowHelpUtil;
 
 /**<p>
- * Obfuscate password files so they are harder to leak information. This is not
+ * Obfuscate password files so they are harder to make leak information. This is not
  * to be confused with encryption. Obfuscation just makes it harder for someone
  * to easily see the password if they have read access to the file.</p>
  * 
- * <p>To use this, put a plain text password in a file, then pass that in as -f <filename>. The
- * program will obfuscate the password. </p>
+ * <p>To use this, put a plain text password in a file, then pass that file name in as -f <filename>. The
+ * program will obfuscate the password in the file. </p>
  * 
- * <p>The key is compiled into the program, so it is portable. To make the obfuscation more
+ * <p>The key used for this is compiled into the program, so it is portable. To make the obfuscation more
  * specific to your system, put a file called "salt.txt" into your home folder or into the
- * top level of the classpath for this program (for example, by putting it into utility-apps.jar).</p>
+ * top level of the classpath for this program (for example, by putting it into utility-apps.jar). Warning:
+ * this does make your program non-portable.</p>
  * 
- * <p>This app will attempt to set the file permissions to read-only for the
- * current user.</p>
+ * <p>This app will attempt to set the file permissions to read-only for the current user.</p>
  * 
  * @author Dave
  * 
@@ -52,24 +53,26 @@ public class ObfuscateApp {
 		} else {
 			File f = new File(passwordFile);
 			if (!f.exists()) {
-				throw new RuntimeException("File does not exist: "
-						+ passwordFile);
+				throw new RuntimeException("File does not exist: "+ passwordFile);
 			}
 			SensitiveBytes bytes;
+		
 			try {
+				
+				// where our stuff goes
+				File propFile = new File(f.getCanonicalPath()+".properties");
+				
 				bytes = new SensitiveBytes(Files.readAllBytes(f.toPath()));
 				bytes.trim();
-				String passwordBase64 = Obfuscate.FACTORY.encrypt(bytes
-						.getData());
+				String passwordBase64 = Obfuscate.FACTORY.encrypt(bytes.getData());
 				Properties props = Properties.Factory.getInstance();
 				props.put("password", passwordBase64);
 				OutputAdapter out = new OutputAdapter(props);
-				out.writeTo(f, new PlainOutputFormat());
+				out.writeTo(propFile, new PlainOutputFormat(), Charset.forName("UTF-8"));
 				f.setExecutable(false);
 				f.setWritable(false);
 				f.setReadable(true, true);
-				System.out.println("Wrote password file to "
-						+ f.getCanonicalPath());
+				System.out.println("Wrote password file to "+ propFile.getCanonicalPath());
 			} catch (IOException e) {
 				throw new RuntimeException(e);
 			}
