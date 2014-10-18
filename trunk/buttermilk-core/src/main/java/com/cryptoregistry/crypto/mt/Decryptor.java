024c53d7b71b6bf3d1c89669c92e4e937acb029c
davesmith.gbs@gmail.com <davesmith.gbs@gmail.com@c95ee3aa-bd1a-c76a-db35-d8dc5d9f496b>
2014-10-18 08:36:10 +0000

    added multi-threaded aes/gcm service engine

--- trunk/buttermilk-core/src/main/java/com/cryptoregistry/crypto/mt/Decryptor.java
+++ trunk/buttermilk-core/src/main/java/com/cryptoregistry/crypto/mt/Decryptor.java
@@ -1,7 +1,18 @@
+/*
+ *  This file is part of Buttermilk
+ *  Copyright 2011-2014 David R. Smith All Rights Reserved.
+ *
+ */
 package com.cryptoregistry.crypto.mt;
 
 import java.util.concurrent.Callable;
 
+/**
+ * This is like a Runnable wrapper for a decryption operation
+ * 
+ * @author Dave
+ *
+ */
 public class Decryptor extends AESGCM_MT implements Callable<Segment> {
 	
 	final Segment segment;
--- trunk/buttermilk-core/src/main/java/com/cryptoregistry/crypto/mt/Encryptor.java
+++ trunk/buttermilk-core/src/main/java/com/cryptoregistry/crypto/mt/Encryptor.java
@@ -1,6 +1,18 @@
+/*
+ *  This file is part of Buttermilk
+ *  Copyright 2011-2014 David R. Smith All Rights Reserved.
+ *
+ */
 package com.cryptoregistry.crypto.mt;
 
 import java.util.concurrent.Callable;
+
+/**
+ * This is like a Runnable wrapper for an encryption operation
+ * 
+ * @author Dave
+ *
+ */
 
 public class Encryptor extends AESGCM_MT implements Callable<Segment> {
 	
