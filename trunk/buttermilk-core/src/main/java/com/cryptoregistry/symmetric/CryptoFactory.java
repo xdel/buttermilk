20017815ac4eafabbf680c0a031b69637ccde54f
davesmith.gbs@gmail.com <davesmith.gbs@gmail.com@c95ee3aa-bd1a-c76a-db35-d8dc5d9f496b>
2014-11-24 09:45:38 +0000

    added Symmetric key format support

--- trunk/buttermilk-core/src/main/java/com/cryptoregistry/symmetric/CryptoFactory.java
+++ trunk/buttermilk-core/src/main/java/com/cryptoregistry/symmetric/CryptoFactory.java
@@ -5,10 +5,10 @@ import java.security.SecureRandom;
 import java.util.concurrent.locks.ReentrantLock;
 
 import com.cryptoregistry.passwords.Password;
-import com.cryptoregistry.passwords.SensitiveBytes;
 
 /**
- * The symmetric CryptoFactory allows for the creation of protected keys for use with AES, etc.
+ * The symmetric CryptoFactory allows for the creation of protected keys for use
+ * with AES, etc.
  * 
  * @author Dave
  *
@@ -17,7 +17,7 @@ public class CryptoFactory {
 
 	private final ReentrantLock lock;
 	private final SecureRandom rand;
-	
+
 	public static final CryptoFactory INSTANCE = new CryptoFactory();
 
 	private CryptoFactory() {
@@ -28,13 +28,31 @@ public class CryptoFactory {
 			throw new RuntimeException(e);
 		}
 	}
+
+	public SymmetricKeyContents generateKey(int size, Password password) {
+		lock.lock();
+		try {
+			if (size < 1)
+				throw new RuntimeException("Size looks wrong.");
+			byte[] bytes = new byte[size];
+			rand.nextBytes(bytes);
+			return new SymmetricKeyContents(
+					SymmetricKeyMetadata.createSecureDefault(password
+							.getPassword()), bytes);
+		} finally {
+			lock.unlock();
+		}
+	}
 	
-	public SymmetricKeyContents generateKey(int size, Password password){
-		if(size<1) throw new RuntimeException("Size looks wrong.");
-		byte [] bytes = new byte[size];
-		rand.nextBytes(bytes);
-		
-		return null;
+	public SymmetricKeyContents wrap(byte[]bytes, Password password) {
+		lock.lock();
+		try {
+			return new SymmetricKeyContents(
+					SymmetricKeyMetadata.createSecureDefault(password
+							.getPassword()), bytes);
+		} finally {
+			lock.unlock();
+		}
 	}
 
 }
---
+++ trunk/buttermilk-core/src/test/java/com/cryptoregistry/symmetric/SymmetricKeyFormatTest.java
@@ -0,0 +1,31 @@
+package com.cryptoregistry.symmetric;
+
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.cryptoregistry.formats.JSONFormatter;
+import com.cryptoregistry.formats.simplereader.JSONSymmetricKeyReader;
+import com.cryptoregistry.passwords.NewPassword;
+
+public class SymmetricKeyFormatTest {
+
+	@Test
+	public void test0() {
+		char[] p = {'p','a','s','s'};
+		SymmetricKeyContents s = CryptoFactory.INSTANCE.generateKey(256, new NewPassword(p));
+		JSONFormatter format = new JSONFormatter("Chinese Knees");
+		format.add(s);
+		StringWriter writer = new StringWriter();
+		format.format(writer);
+		String out = writer.toString();
+		JSONSymmetricKeyReader reader = new JSONSymmetricKeyReader(new StringReader(out),new NewPassword(p));
+		SymmetricKeyContents contents = reader.parse();
+		//System.out.println(contents);
+		Assert.assertNotNull(contents);
+		
+	}
+
+}
