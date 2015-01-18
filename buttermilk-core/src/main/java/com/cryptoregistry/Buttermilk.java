b161b3d6fc3abe0a22fa7fac06fa7d41ebf7d139
davesmith.gbs <davesmith.gbs@gmail.com>
2015-01-18 06:04:51 +0000

    added license block

--- buttermilk-core/src/main/java/com/cryptoregistry/Buttermilk.java
+++ buttermilk-core/src/main/java/com/cryptoregistry/Buttermilk.java
@@ -1,3 +1,8 @@
+/*
+ *  This file is part of Buttermilk
+ *  Copyright 2011-2014 David R. Smith All Rights Reserved.
+ *
+ */
 package com.cryptoregistry;
 
 import com.cryptoregistry.c2.key.C2KeyMetadata;
