a91cd23143e892efc8545ebe5728085f3624d0c5
davesmith.gbs <davesmith.gbs@gmail.com>
2015-01-25 01:13:01 +0000

    work towards bTLS

--- buttermilk-core/src/main/java/x/org/bouncycastle/crypto/io/DigestOutputStream.java
+++ buttermilk-core/src/main/java/x/org/bouncycastle/crypto/io/DigestOutputStream.java
@@ -1,42 +1,47 @@
 package x.org.bouncycastle.crypto.io;
 
+import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
 import x.org.bouncycastle.crypto.Digest;
 
-public class DigestOutputStream
-    extends OutputStream
-{
-    protected Digest digest;
+/**
+ * This has been updated to support bTLS purposes
+ * 
+ * @author Dave
+ *
+ */
+public class DigestOutputStream extends FilterOutputStream {
+	protected Digest digest;
 
-    public DigestOutputStream(
-        Digest          Digest)
-    {
-        this.digest = Digest;
-    }
+	public DigestOutputStream(OutputStream out, Digest digest) {
+		super(out);
+		this.digest = digest;
+	}
 
-    public void write(int b)
-        throws IOException
-    {
-        digest.update((byte)b);
-    }
+	public void write(int b) throws IOException {
+		digest.update((byte) b);
+		out.write(b);
+	}
 
-    public void write(
-        byte[] b,
-        int off,
-        int len)
-        throws IOException
-    {
-        digest.update(b, off, len);
-    }
+	public void write(byte[] b, int off, int len) throws IOException {
+		digest.update(b, off, len);
+		out.write(b, off, len);
+	}
 
-    public byte[] getDigest()
-    {
-        byte[] res = new byte[digest.getDigestSize()];
-        
-        digest.doFinal(res, 0);
-        
-        return res;
-    }
+	public void write(byte[] b) throws IOException {
+		digest.update(b, 0, b.length);
+		out.write(b);
+	}
+
+	public byte[] getDigest() {
+		byte[] res = new byte[digest.getDigestSize()];
+		digest.doFinal(res, 0);
+		return res;
+	}
+
+	public void reset() {
+		digest.reset();
+	}
 }
