2f5f7efc22bd39abb9e875b6fde301382bd2c5a3
davesmith.gbs@gmail.com <davesmith.gbs@gmail.com@c95ee3aa-bd1a-c76a-db35-d8dc5d9f496b>
2015-01-25 01:13:01 +0000

    work towards bTLS

--- trunk/buttermilk-core/src/main/java/x/org/bouncycastle/crypto/io/DigestInputStream.java
+++ trunk/buttermilk-core/src/main/java/x/org/bouncycastle/crypto/io/DigestInputStream.java
@@ -6,47 +6,53 @@ import java.io.InputStream;
 
 import x.org.bouncycastle.crypto.Digest;
 
-public class DigestInputStream
-    extends FilterInputStream
-{
-    protected Digest digest;
+/**
+ * This has been updated to support bTLS purposes
+ * 
+ * @author Dave
+ *
+ */
 
-    public DigestInputStream(
-        InputStream stream,
-        Digest      digest)
-    {
-        super(stream);
-        this.digest = digest;
-    }
+public class DigestInputStream extends FilterInputStream {
+	protected Digest digest;
 
-    public int read()
-        throws IOException
-    {
-        int b = in.read();
+	public DigestInputStream(InputStream stream, Digest digest) {
+		super(stream);
+		this.digest = digest;
+	}
 
-        if (b >= 0)
-        {
-            digest.update((byte)b);
-        }
-        return b;
-    }
+	public int read() throws IOException {
+		int b = in.read();
 
-    public int read(
-        byte[] b,
-        int off,
-        int len)
-        throws IOException
-    {
-        int n = in.read(b, off, len);
-        if (n > 0)
-        {
-            digest.update(b, off, n);
-        }
-        return n;
-    }
+		if (b >= 0) {
+			digest.update((byte) b);
+		}
+		return b;
+	}
 
-    public Digest getDigest()
-    {
-        return digest;
-    }
+	public int read(byte[] b, int off, int len) throws IOException {
+		int n = in.read(b, off, len);
+		if (n > 0) {
+			digest.update(b, off, n);
+		}
+		return n;
+	}
+
+	public int read(byte[] b) throws IOException {
+		int n = in.read(b);
+		if (n > 0) {
+			digest.update(b, 0, n);
+		}
+		return n;
+	}
+
+	public byte[] getDigest() {
+		byte[] res = new byte[digest.getDigestSize()];
+		digest.doFinal(res, 0);
+		return res;
+	}
+	
+	public void reset() {
+		digest.reset();
+	}
 }
