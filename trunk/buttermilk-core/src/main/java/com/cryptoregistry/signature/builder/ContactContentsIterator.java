ffb87ff963735d220e9eac3d412198578d4ef61c
davesmith.gbs@gmail.com <davesmith.gbs@gmail.com@c95ee3aa-bd1a-c76a-db35-d8dc5d9f496b>
2015-02-19 09:48:14 +0000

    work towards utilities

--- trunk/buttermilk-core/src/main/java/com/cryptoregistry/signature/builder/ContactContentsIterator.java
+++ trunk/buttermilk-core/src/main/java/com/cryptoregistry/signature/builder/ContactContentsIterator.java
@@ -20,7 +20,7 @@ public class ContactContentsIterator implements MapIterator {
 	public ContactContentsIterator(CryptoContact contact) {
 		super();
 		this.contact = contact;
-		iter = contact.getMap().keySet().iterator();
+		iter = contact.iterator();
 		handleLength = contact.getHandle().length()+1;
 	}
 	
@@ -52,14 +52,14 @@ public class ContactContentsIterator implements MapIterator {
 
 	public String get(String key){
 		if(key.startsWith("."))
-		return contact.getMap().get(key.substring(1,key.length()));
-		else return contact.getMap().get(key.substring(handleLength,key.length()));
+		return contact.get(key.substring(1,key.length()));
+		else return contact.get(key.substring(handleLength,key.length()));
 	}
 	
 	public void reset(){
 		if(!iter.hasNext()) {
 			index = 0;
-			iter = contact.getMap().keySet().iterator();
+			iter = contact.iterator();
 		}
 	}
 
