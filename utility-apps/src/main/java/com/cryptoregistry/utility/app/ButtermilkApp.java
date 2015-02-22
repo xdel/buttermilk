ae6f3ef996c0606355f2995b5833e94f236a3725
davesmith.gbs <davesmith.gbs@gmail.com>
2015-02-22 04:43:44 +0000

    work towards utilities

--- utility-apps/src/main/java/com/cryptoregistry/utility/app/ButtermilkApp.java
+++ utility-apps/src/main/java/com/cryptoregistry/utility/app/ButtermilkApp.java
@@ -1,28 +1,86 @@
 package com.cryptoregistry.utility.app;
 
-import java.io.IOException;
-
 import com.cryptoregistry.util.CmdLineParser;
 import com.cryptoregistry.util.CmdLineParser.Option;
 import com.cryptoregistry.util.CmdLineParser.OptionException;
-
+import com.cryptoregistry.util.ShowHelpUtil;
 
 public class ButtermilkApp {
 
-public static final void main(String[] argv) {
+	CmdLineParser parser;
+	
+	private void backup(String [] args){
 		
-		CmdLineParser parser = new CmdLineParser();
-		Option<String> fileOpt = parser.addStringOption('f', "file");
-		Option<String> argOpt = parser.addStringOption('a', "arg");
-		Option<String> charsetOpt = parser.addStringOption('c', "charset");
-		
-		try {
-			parser.parse(argv);
-			
-			
-		} catch (OptionException e) {
-			e.printStackTrace();
-		} 
 	}
 
+	private void contact(String [] args){
+		
+	}
+	
+	public static final void main(String[] argv) {
+
+		if (argv.length == 0) {
+			showHelp();
+			return;
+		}
+		
+		ButtermilkApp app = new ButtermilkApp();
+		
+		String anchor = argv[0];
+		String[] parameters = new String[argv.length - 1];
+		System.arraycopy(argv, 0, parameters, 0, argv.length);
+		switch (anchor) {
+
+			case "backup": {
+				app.backup(parameters);
+				break;
+			}
+			case "contact": {
+				app.contact(parameters);
+				break;
+			}
+			case "datastore": {
+	
+				break;
+			}
+			case "export": {
+	
+				break;
+			}
+			case "import": {
+	
+				break;
+			}
+			case "key": {
+	
+				break;
+			}
+			case "list": {
+	
+				break;
+			}
+			case "password": {
+	
+				break;
+			}
+			case "sign": {
+	
+				break;
+			}
+			case "validate": {
+	
+				break;
+			}
+			default:
+				error("Unknown anchor: " + anchor);
+			}
+	}
+
+	private static final void showHelp() {
+		ShowHelpUtil.help("/buttermilk-help-message.txt");
+	}
+
+	private static final void error(String txt) {
+		System.err.println(txt);
+	}
 }
