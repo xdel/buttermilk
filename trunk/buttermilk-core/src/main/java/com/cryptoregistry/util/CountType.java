42e1245892b1feb74643de158b8cbd44cea3ea19
davesmith.gbs@gmail.com <davesmith.gbs@gmail.com@c95ee3aa-bd1a-c76a-db35-d8dc5d9f496b>
2015-03-22 09:09:33 +0000

    work towards bTLS

---
+++ trunk/buttermilk-core/src/main/java/com/cryptoregistry/util/CountType.java
@@ -0,0 +1,10 @@
+/*
+ *  This file is part of Buttermilk(TM) 
+ *  Copyright 2013 David R. Smith for cryptoregistry.com
+ *
+ */
+package com.cryptoregistry.util;
+
+public enum CountType {
+	DOT,HYPHEN,SPACE;
+}
