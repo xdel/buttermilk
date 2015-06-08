43c2c556db4b2af0a708930fd84c1d4a9517e9c8
davesmith.gbs@gmail.com <davesmith.gbs@gmail.com@c95ee3aa-bd1a-c76a-db35-d8dc5d9f496b>
2015-06-08 04:58:42 +0000

    updated SignatureFormatter

--- trunk/buttermilk-core/src/main/java/com/cryptoregistry/formats/SignatureFormatter.java
+++ trunk/buttermilk-core/src/main/java/com/cryptoregistry/formats/SignatureFormatter.java
@@ -21,23 +21,29 @@ import com.fasterxml.jackson.core.JsonGenerator;
 public class SignatureFormatter {
 
 	private List<CryptoSignature> list;
-	
+
 	public SignatureFormatter() {
 		list = new ArrayList<CryptoSignature>();
+	}
+	
+	public SignatureFormatter(CryptoSignature sig) {
+		list = new ArrayList<CryptoSignature>();
+		list.add(sig);
 	}
 
 	public SignatureFormatter(List<CryptoSignature> list) {
 		super();
 		this.list = list;
 	}
-	
-	public void add(CryptoSignature sig){
+
+	public void add(CryptoSignature sig) {
 		list.add(sig);
 	}
-	
-	public void format(JsonGenerator g, Writer writer) throws JsonGenerationException, IOException{
-		for(CryptoSignature s: list){
-			
+
+	public void format(JsonGenerator g, Writer writer)
+			throws JsonGenerationException, IOException {
+		for (CryptoSignature s : list) {
+
 			// common stuff
 			g.writeObjectFieldStart(s.getHandle());
 			g.writeStringField("CreatedOn", TimeUtil.format(s.getCreatedOn()));
@@ -45,14 +51,14 @@ public class SignatureFormatter {
 			g.writeStringField("SignedBy", s.getSignedBy());
 			g.writeStringField("SignatureAlgorithm", s.getSigAlg().toString());
 			g.writeStringField("DigestAlgorithm", s.metadata.digestAlg);
-			
+
 			// does the actual key contents (possibly multiple fields)
 			s.formatSignaturePrimitivesJSON(g, writer);
-			
+
 			// also common
 			g.writeArrayFieldStart("DataRefs");
 			Iterator<String> iter = s.getDataRefs().iterator();
-			while(iter.hasNext()){
+			while (iter.hasNext()) {
 				g.writeString(iter.next());
 			}
 			g.writeEndArray();
@@ -61,12 +67,37 @@ public class SignatureFormatter {
 	}
 	
 	public String format(){
+		return this.format(0);
+	}
+
+	public String format(int index) {
+		CryptoSignature s = list.get(index);
 		StringWriter writer = new StringWriter();
 		JsonFactory f = new JsonFactory();
 		JsonGenerator g = null;
 		try {
 			g = f.createGenerator(writer);
-			format(g, writer);
+			
+			g.writeStartObject();
+			
+			g.writeObjectFieldStart(s.getHandle());
+			g.writeStringField("CreatedOn", TimeUtil.format(s.getCreatedOn()));
+			g.writeStringField("SignedWith", s.getSignedWith());
+			g.writeStringField("SignedBy", s.getSignedBy());
+			g.writeStringField("SignatureAlgorithm", s.getSigAlg().toString());
+			g.writeStringField("DigestAlgorithm", s.metadata.digestAlg);
+
+			// does the actual key contents (possibly multiple fields)
+			s.formatSignaturePrimitivesJSON(g, writer);
+
+			// also common
+			g.writeArrayFieldStart("DataRefs");
+			Iterator<String> iter = s.getDataRefs().iterator();
+			while (iter.hasNext()) {
+				g.writeString(iter.next());
+			}
+			g.writeEndArray();
+			g.writeEndObject();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
---
+++ trunk/buttermilk-core/src/test/java/com/cryptoregistry/formats/SignatureFormatterTest.java
@@ -0,0 +1,32 @@
+package com.cryptoregistry.formats;
+
+import java.nio.charset.Charset;
+import java.util.Formatter;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.cryptoregistry.c2.CryptoFactory;
+import com.cryptoregistry.c2.key.Curve25519KeyContents;
+import com.cryptoregistry.signature.C2CryptoSignature;
+
+public class SignatureFormatterTest {
+
+	@Test
+	public void test() {
+		String signedBy = "Chinese Eyes"; // my registration handle
+		String message = "My message text...";
+		byte[] msgBytes = message.getBytes(Charset.forName("UTF-8"));
+
+		Curve25519KeyContents c2Keys = CryptoFactory.INSTANCE.generateKeys();
+		C2CryptoSignature sig = CryptoFactory.INSTANCE.sign(signedBy, c2Keys,
+				msgBytes);
+		String desc = new SignatureFormatter(sig).format();
+		System.err.println(desc);
+		boolean ok = CryptoFactory.INSTANCE.verify(c2Keys, msgBytes, sig.getSignature());
+		Assert.assertTrue(ok);
+
+	}
+
+}
