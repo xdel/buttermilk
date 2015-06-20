9873ff9fe0f47c583c722c9ac264348d23f5f159
davesmith.gbs <davesmith.gbs@gmail.com>
2015-06-20 02:43:20 +0000

    updates

---
+++ buttermilk-core/src/main/java/com/cryptoregistry/formats/simplereader/pub/JSONC2KeyForPublicationReader.java
@@ -0,0 +1,89 @@
+/*
+ *  This file is part of Buttermilk
+ *  Copyright 2011-2014 David R. Smith All Rights Reserved.
+ *
+ */
+package com.cryptoregistry.formats.simplereader.pub;
+
+import java.io.Reader;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
+
+import com.cryptoregistry.c2.key.C2KeyMetadata;
+import com.cryptoregistry.c2.key.Curve25519KeyForPublication;
+import com.cryptoregistry.c2.key.PublicKey;
+import com.cryptoregistry.formats.EncodingHint;
+import com.cryptoregistry.formats.KeyFormat;
+import com.cryptoregistry.formats.Mode;
+import com.cryptoregistry.util.ArmoredString;
+import com.cryptoregistry.util.TimeUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Given a map of key data in unlocked condition, create a C2 style key from that
+ * 
+ * @author Dave
+ *
+ */
+public class JSONC2KeyForPublicationReader {
+	
+	protected final ObjectMapper mapper;
+	protected final Map<String,Object> map;
+	 
+	@SuppressWarnings("unchecked")
+	public JSONC2KeyForPublicationReader(Reader in) {
+		mapper = new ObjectMapper();
+		try {
+			map = mapper.readValue(in, Map.class);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public JSONC2KeyForPublicationReader(String in) {
+		mapper = new ObjectMapper();
+		try {
+			map = mapper.readValue(in, Map.class);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Curve25519KeyForPublication parse() {
+		Iterator<String> iter = map.keySet().iterator();
+		if(iter.hasNext()){
+			String distUUID = iter.next();
+			Map<String,Object> inner = (Map<String,Object>) map.get(distUUID);
+			if(distUUID.endsWith("-P")){
+			//	String keyAlgorithm = String.valueOf(inner.get("KeyAlgorithm"));
+				Date createdOn = TimeUtil.getISO8601FormatDate(String.valueOf(inner.get("CreatedOn")));
+				
+				// at the moment these are always base64url
+				EncodingHint enc =	EncodingHint.valueOf(String.valueOf(inner.get("Encoding")));
+				if(enc != EncodingHint.Base64url) throw new RuntimeException("Unexpected encoding, needs to be Base64url");
+				
+				ArmoredString P = new ArmoredString(String.valueOf(inner.get("P")));
+				
+				
+				C2KeyMetadata meta = new C2KeyMetadata(
+						distUUID.substring(0,distUUID.length()-2),
+						createdOn,
+						new KeyFormat(enc,Mode.FOR_PUBLICATION,null)
+				);
+				
+				PublicKey pKey = new PublicKey(P.decodeToBytes());
+				
+				return new Curve25519KeyForPublication(meta,pKey);
+				
+			}else{
+				throw new RuntimeException("unexpected Mode, needs to be ForPublication");
+			}
+		}else{
+			throw new RuntimeException("Count not find the uuid, fail");
+		}
+	}
+
+}
---
+++ buttermilk-core/src/main/java/com/cryptoregistry/formats/simplereader/pub/JSONRSAKeyForPublicationReader.java
@@ -0,0 +1,112 @@
+/*
+ *  This file is part of Buttermilk
+ *  Copyright 2011-2014 David R. Smith All Rights Reserved.
+ *
+ */
+package com.cryptoregistry.formats.simplereader.pub;
+
+import java.io.Reader;
+import java.math.BigInteger;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
+
+import com.cryptoregistry.formats.EncodingHint;
+import com.cryptoregistry.formats.FormatUtil;
+import com.cryptoregistry.formats.KeyFormat;
+import com.cryptoregistry.formats.Mode;
+import com.cryptoregistry.rsa.RSAKeyForPublication;
+import com.cryptoregistry.rsa.RSAKeyMetadata;
+import com.cryptoregistry.util.TimeUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * <p>
+ * JSONReader's design works on the premise the reader instance knows nothing about the key
+ * materials it is reading; the file could contain 1 key or 100 and a lot of other stuff
+ * besides. But sometimes you just need a very simple reader which assumes the 
+ * program knows at design time the nature of the key to be read - i.e., the programmer knows
+ * what kind of keys she is using and can rely on the type. 
+ * </p>
+ * 
+ * <p>
+ * While less general, this approach allows the programmer to avoid the process of testing 
+ * the runtime object to find the type which was parsed using the "KeyMaterials" semantics. 
+ * In other words, it is a simple, efficient, direct approach.  
+ * </p>
+ * 
+ * <p>This class assumes you are reading a very simple key file with no associated signatures
+ *  essentially like the below. It can read only the For Publication mode of an RSA Key.
+ * </p>
+ * 
+ * <pre>
+ 
+ For publication:
+ 
+ 
+
+ </pre>
+ * 
+ * 
+ * @author Dave
+ * @see JSONFormatter
+ * 
+ */
+public class JSONRSAKeyForPublicationReader {
+	
+	protected final ObjectMapper mapper;
+	protected final Map<String,Object> map;
+	 
+	@SuppressWarnings("unchecked")
+	public JSONRSAKeyForPublicationReader(Reader in) {
+		mapper = new ObjectMapper();
+		try {
+			map = mapper.readValue(in, Map.class);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public JSONRSAKeyForPublicationReader(String in) {
+		mapper = new ObjectMapper();
+		try {
+			map = mapper.readValue(in, Map.class);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public RSAKeyForPublication parse() {
+		Iterator<String> iter = map.keySet().iterator();
+		if (iter.hasNext()) {
+			String distUUID = iter.next();
+			Map<String, Object> inner = (Map<String, Object>) map.get(distUUID);
+			if (distUUID.endsWith("-P")) {
+				Date createdOn = TimeUtil.getISO8601FormatDate(String
+						.valueOf(inner.get("CreatedOn")));
+				
+				EncodingHint enc = EncodingHint.valueOf(String.valueOf(inner.get("Encoding")));
+				int strength = Integer.parseInt(String.valueOf(inner.get("Strength")));
+				BigInteger Modulus = FormatUtil.unwrap(enc, String.valueOf(inner.get("Modulus")));
+				BigInteger publicExponent = FormatUtil.unwrap(enc, String.valueOf(inner.get("PublicExponent")));
+
+				RSAKeyMetadata meta = new RSAKeyMetadata(
+						distUUID.substring(0, distUUID.length() - 2), 
+						createdOn, 
+						new KeyFormat(enc, Mode.FOR_PUBLICATION, null)
+				);
+				
+				meta.setStrength(strength);
+
+				return new RSAKeyForPublication(meta, Modulus,publicExponent);
+
+			} else {
+				throw new RuntimeException("unexpected Mode, needs to be For Publication");
+			}
+		} else {
+			throw new RuntimeException("Count not find the uuid, fail");
+		}
+	}
+}
